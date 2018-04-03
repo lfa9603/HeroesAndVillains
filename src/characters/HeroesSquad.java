bb078575a1b5d444db5a35bc100bb9e07b4f241c
lfa9603 <faso.9603@gmail.com>
2018-04-03 16:37:03 +1200

    Added HeroSquad.java

---
+++ src/characters/HeroesSquad.java
@@ -0,0 +1,17 @@
+package characters;
+
+import java.util.ArrayList;
+
+public class HeroesSquad extends Squad {
+
+	public HeroesSquad(ArrayList<Character> characters) {
+		super(characters);
+	}
+	
+	@Override
+	public Character getPrimaryCharacter() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/characters/Squad.java
+++ src/characters/Squad.java
@@ -18,4 +18,6 @@ public abstract class Squad {
 		this.characters = characters;
 	}
 	
+	public abstract Character getPrimaryCharacter();
+	
 }
