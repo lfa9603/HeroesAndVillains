78215babca179c045e7038c4b37f5813d7d71709
Jay <janardan.hamilton@gmail.com>
2018-04-04 14:31:31 +1200

    Added AddHero to HeroSquad

--- src/characters/HeroesSquad.java
+++ src/characters/HeroesSquad.java
@@ -2,28 +2,35 @@ package characters;
 
 import java.util.ArrayList;
 
+import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
 /**
  * 
  * @author LorenzoFasano and JayHamilton
  *Collection of Character objects stored in an ArrayList 
  */
 public class HeroesSquad {
-	private ArrayList<Hero> HeroSquad = new ArrayList<Hero>();
+	private ArrayList<Hero> heroSquad = new ArrayList<Hero>();
 
 	
 	/**
 	 * @return the heroSquad
 	 */
 	public ArrayList<Hero> getHeroSquad() {
-		return HeroSquad;
+		return heroSquad;
 	}
 
 
 	/**
-	 * @param Checks if hero is in the Squad and adds it, else it return the
+	 * @param Checks if hero is in the Squad and adds it, else it raises an error.
 	 */
-	public void setHeroSquad(ArrayList<Hero> heroSquad) {
-		HeroSquad = heroSquad;
+	public void addHero(Hero hero) {
+		 if (!heroSquad.contains(hero)){
+			 heroSquad.add(hero);
+		 }
+		 else {
+			 throw new ValueException("Heros cannot have the Same name");
+		 }
 	}
 
 
