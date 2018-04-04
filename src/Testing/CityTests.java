9608e7e10229865cfe7bec2f1529ca277b756dd2
lfa9603 <faso.9603@gmail.com>
2018-04-04 15:15:07 +1200

    Implemented code for the city package

--- src/Testing/CityTests.java
+++ src/Testing/CityTests.java
@@ -2,13 +2,87 @@ package Testing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.Point;
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
+
+import city.Coordinates;
+import city.buildings.Building;
+import city.buildings.Hospital;
+import city.buildings.PowerUpDen;
+import city.buildings.Shop;
+import city.buildings.VillainsLair;
 
 class CityTests {
 
+	
+	/**
+	 * Testing the static value HOME_COORDINATES
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testingHOME_COORDINATES() {
+		Point home = Coordinates.HOME_COORDINATES;
+		Point origin = new Point(0, 0);
+		assertEquals(origin, home);
+	}
+	
+	/**
+	 * Testing the static value BUILDING_COORDINATES
+	 */
+	@Test
+	void testingBUILDING_COORDINATES() {
+		ArrayList<Point> toTest = Coordinates.BUILDINGS_COORDINATES;
+		ArrayList<Point> buildingCoordinates = new ArrayList<Point>();
+		buildingCoordinates.add(new Point(4, 0));
+		buildingCoordinates.add(new Point(0, 4));
+		buildingCoordinates.add(new Point(-4, 0));
+		buildingCoordinates.add(new Point(0, -4));
+		assertEquals(toTest, buildingCoordinates);
+	}
+	
+	/**
+	 * Testing the static function setBuildingsCoordinates
+	 * This is achieved instantiating an ArrayList<Building> and then pass it to
+	 * the function, this should assign a random set of coordinates
+	 * to each building.
+	 * The NonNull and NotEquals properties are checked comparing all the items with each other.
+	 */
+	@Test
+	void testingSetBuildingsCoordinates() {
+		Hospital hospital = new Hospital("Hospital");
+		PowerUpDen pub = new PowerUpDen("Pub");
+		VillainsLair villainCave = new VillainsLair("VillainCave");
+		Shop shop = new Shop("Shop");
+		
+		ArrayList<Building> listBuildings = new ArrayList<Building>();
+		
+		listBuildings.add(pub);
+		listBuildings.add(shop);
+		listBuildings.add(villainCave);
+		listBuildings.add(hospital);
+		
+		Coordinates.setBuildingsCoordinates(listBuildings);
+		
+		boolean allDifferentAndNotNull = true;
+		for (Building buildingOne : listBuildings) {
+			for (Building buildingTwo : listBuildings) {
+				Point coordinatesOne = buildingOne.getBuildingCoordinates();
+				Point coordinatesTwo = buildingTwo.getBuildingCoordinates();
+				if (coordinatesOne.equals(coordinatesTwo) && !buildingOne.equals(buildingTwo)) {
+					allDifferentAndNotNull = false;
+				}
+				if (coordinatesOne.equals(null) || coordinatesTwo.equals(null))  {
+					allDifferentAndNotNull = false;
+				}
+			}
+		}
+		
+		boolean expected = true;
+		for (Building building : listBuildings) {
+			System.out.println(building.getBuildingName() + "    " + building.getBuildingCoordinates());
+		}
+		assertEquals(expected, allDifferentAndNotNull);
 	}
 
 }
