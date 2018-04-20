fd24a9b5b3e1600bfb6ec5eab11221cfd2c5a563
Jay <janardan.hamilton@gmail.com>
2018-04-20 13:16:54 +1200

    further changes to Minigame V2

--- src/city/package-info.java
+++ src/city/package-info.java
@@ -2,9 +2,16 @@
  * 
  */
 /**
+ * 
  * @author LorenzoFasano
- * This package is used to gather all the tools and objects needed to create the different cities, 
- * the map and the overall structure of the surrounding.
+ * This package contains: City.java a class that defines the overall structure of each level, 
+ * CoordinatesTool.java which contains static methods
+ * which help the construction of each City object and WorldBuilder.java which deals with the 
+ * user input an creates an ArrayList of City objects (the  levels the player will have to 
+ * complete to finish the game).
+ *
+ * It also contains a sub-package buildings which contains all objects (Building objects) needed to 
+ * construct a city and the respective helper classes.  
  *
  */
 package city;
