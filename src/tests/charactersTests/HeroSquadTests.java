ebfd09b10a61c9c055a30ba8bb039b66a8a88ef9
lfa9603 <faso.9603@gmail.com>
2018-04-25 20:01:25 +1200

    Added test cases to HeroesSquadTests

--- src/tests/charactersTests/HeroSquadTests.java
+++ src/tests/charactersTests/HeroSquadTests.java
@@ -10,6 +10,11 @@ import characters.Abilities;
 import characters.Hero;
 import characters.HeroesSquad;
 import characters.Types;
+import city.City;
+import collectables.CollectableID;
+import collectables.Inventory;
+import collectables.healingItem.HealingItem;
+import collectables.heroesMap.HeroesMap;
 
 class HeroSquadTests {
 
@@ -28,6 +33,7 @@ class HeroSquadTests {
 		heroes.addHero(hero1);
 		heroes.addHero(hero2);
 		heroes.addHero(hero3);
+		
 	}
 
 	@Test
@@ -39,6 +45,62 @@ class HeroSquadTests {
 		assertTrue(heroes.squadContains(hero1));
 		assertTrue(heroes.squadContains(hero2));
 		assertTrue(heroes.squadContains(hero3));
+	}
+	
+	@Test
+	void squadResetTest() {
+		heroes.squadReset();
+		assertEquals(heroes.getHeroSquad().size(), 0);
+	}
+	
+	@Test
+	void getterAndSetterForTeamName() {
+		heroes.setTeamName("CiaoBellaDonna");
+		assertEquals("CiaoBellaDonna", heroes.getTeamName());
+	}
+	
+	@Test
+	void setterAndGetterHaveMap() {
+		
+		assertFalse(heroes.isHaveMap());
+		assertNull(heroes.getCurrentCity());
+		
+		heroes.setCurrentCity(new City());
+		heroes.setHaveMap(true);
+		
+		assertTrue(heroes.isHaveMap());
+		assertNotNull(heroes.getCurrentCity());
+		
+		
+	}
+	
+	@Test
+	void getHeroTest() {
+		assertEquals("hero1", heroes.getHero(0).getCharacterName());
+		assertEquals("hero2", heroes.getHero(1).getCharacterName());
+		assertEquals("hero3", heroes.getHero(2).getCharacterName());
+	}
+	
+	@Test
+	void getLengthTest() {
+		assertEquals(heroes.getLength(), 3);
+	}
+	
+	@Test
+	void getterAndSetterHeroesMap() {
+		HeroesMap map = new HeroesMap(CollectableID.HeroesMap);
+		heroes.setHeroesMap(map);
+		assertEquals(map, heroes.getHeroesMap());
+	}
+	
+	@Test
+	void getBackpackTest() {
+		assertTrue(heroes.getBackPack().getInventory().isEmpty());
+		
+		heroes.getBackPack().addItemToInventory(new HealingItem(CollectableID.GoodHealingItem));
+		assertEquals(heroes.getBackPack().getInventory().size(), 1);
+		
+
 	}
 
 	@AfterEach
