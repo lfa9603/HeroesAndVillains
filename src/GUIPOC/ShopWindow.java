fda5b59172a27da7f9b4794847a4546192d87160
lfa9603 <faso.9603@gmail.com>
2018-05-12 15:51:24 +1200

    Kept going with ShopWindow

--- src/GUIPOC/ShopWindow.java
+++ src/GUIPOC/ShopWindow.java
@@ -3,15 +3,13 @@ package GUIPOC;
 import javax.swing.JFrame;
 
 import city.City;
-import city.buildings.PowerUpDen;
+
 import city.buildings.TypeBuildings;
 import city.buildings.shop.Shop;
 import collectables.CollectableID;
 import collectables.powerUp.Armor;
 import collectables.powerUp.GameChooser;
 
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 
 import characters.Abilities;
 import characters.Hero;
@@ -25,6 +23,9 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JToggleButton;
 import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class ShopWindow {
 
@@ -65,6 +66,7 @@ public class ShopWindow {
 		mainWindow = mainWind;
 		
 		initialize();
+		frame.setVisible(true);
 		
 	}
 
@@ -73,38 +75,96 @@ public class ShopWindow {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 759, 657);
+		frame.setBounds(100, 100, 839, 839);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JTextArea txtrWelcome = new JTextArea();
-		txtrWelcome.setWrapStyleWord(true);
-		txtrWelcome.setText("Welcome to the Shop! Make " + shopBuilding.getNameOfInnkeeper() +", the owner, happy spending your money here!");
-		txtrWelcome.setLineWrap(true);
-		txtrWelcome.setEditable(false);
-		txtrWelcome.setBackground(SystemColor.menu);
-		txtrWelcome.setBounds(21, 21, 691, 43);
-		frame.getContentPane().add(txtrWelcome);
+		JTextArea welcomeShopTxtArea = new JTextArea();
+		welcomeShopTxtArea.setWrapStyleWord(true);
+		welcomeShopTxtArea.setText("Welcome to the Shop! Make " + shopBuilding.getNameOfInnkeeper() +", the owner, happy spending your money here!");
+		welcomeShopTxtArea.setLineWrap(true);
+		welcomeShopTxtArea.setEditable(false);
+		welcomeShopTxtArea.setBackground(SystemColor.menu);
+		welcomeShopTxtArea.setBounds(21, 21, 691, 43);
+		frame.getContentPane().add(welcomeShopTxtArea);
 		
 		JToggleButton justBrowsingTgBtn = new JToggleButton("Just Browsing");
-		justBrowsingTgBtn.setBounds(60, 72, 207, 35);
+		justBrowsingTgBtn.setBounds(21, 265, 207, 35);
 		frame.getContentPane().add(justBrowsingTgBtn);
 		
 		JToggleButton talkToInnkeeperTgBtn = new JToggleButton("Talk to " + shopBuilding.getNameOfInnkeeper());
-		talkToInnkeeperTgBtn.setBounds(435, 72, 207, 35);
+		talkToInnkeeperTgBtn.setBounds(464, 265, 207, 35);
 		frame.getContentPane().add(talkToInnkeeperTgBtn);
 		
 		JPanel justBrowsingPanel = new JPanel();
-		justBrowsingPanel.setBounds(21, 129, 313, 382);
+		justBrowsingPanel.setBounds(21, 321, 303, 347);
 		frame.getContentPane().add(justBrowsingPanel);
+		justBrowsingPanel.setLayout(null);
+		
+		JTextArea shopGoodsTxtArea = new JTextArea();
+		shopGoodsTxtArea.setWrapStyleWord(true);
+		shopGoodsTxtArea.setText((String) null);
+		shopGoodsTxtArea.setLineWrap(true);
+		shopGoodsTxtArea.setEditable(false);
+		shopGoodsTxtArea.setBackground(SystemColor.menu);
+		shopGoodsTxtArea.setBounds(21, 21, 271, 305);
+		justBrowsingPanel.add(shopGoodsTxtArea);
 		
 		JPanel talkToInnkeeperPanel = new JPanel();
-		talkToInnkeeperPanel.setBounds(399, 129, 313, 382);
+		talkToInnkeeperPanel.setBounds(464, 321, 313, 347);
 		frame.getContentPane().add(talkToInnkeeperPanel);
+		talkToInnkeeperPanel.setLayout(null);
+		
+		JComboBox allItemsComboBox = new JComboBox();
+		allItemsComboBox.setBounds(21, 104, 124, 32);
+		talkToInnkeeperPanel.add(allItemsComboBox);
+		
+		JLabel chooseItemBuyLabel = new JLabel("Choose item to buy");
+		chooseItemBuyLabel.setBounds(21, 72, 210, 26);
+		talkToInnkeeperPanel.add(chooseItemBuyLabel);
+		
+		JLabel whatCanIGetYaLabel = new JLabel("Hi! what can I get ya?");
+		whatCanIGetYaLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		whatCanIGetYaLabel.setBounds(21, 21, 271, 26);
+		talkToInnkeeperPanel.add(whatCanIGetYaLabel);
+		
+		JButton buyItemBtn = new JButton("Buy Item");
+		buyItemBtn.setBounds(166, 104, 127, 32);
+		talkToInnkeeperPanel.add(buyItemBtn);
+		
+		JTextArea displaySuccessOrRejectionPurchase = new JTextArea();
+		displaySuccessOrRejectionPurchase.setWrapStyleWord(true);
+		displaySuccessOrRejectionPurchase.setText((String) null);
+		displaySuccessOrRejectionPurchase.setLineWrap(true);
+		displaySuccessOrRejectionPurchase.setEditable(false);
+		displaySuccessOrRejectionPurchase.setBackground(SystemColor.menu);
+		displaySuccessOrRejectionPurchase.setBounds(21, 346, 271, 141);
+		talkToInnkeeperPanel.add(displaySuccessOrRejectionPurchase);
 		
 		JButton backToMapBtn = new JButton("Back to the Map! ");
-		backToMapBtn.setBounds(105, 532, 504, 35);
+		backToMapBtn.setBounds(191, 691, 504, 35);
 		frame.getContentPane().add(backToMapBtn);
+		
+		JPanel heroesInfosPanel = new JPanel();
+		heroesInfosPanel.setBounds(21, 56, 771, 188);
+		frame.getContentPane().add(heroesInfosPanel);
+		heroesInfosPanel.setLayout(null);
+		
+		JTextArea txtrSjhrweshjewhrehjriowejroiwjreoiw = new JTextArea();
+		txtrSjhrweshjewhrehjriowejroiwjreoiw.setWrapStyleWord(true);
+		txtrSjhrweshjewhrehjriowejroiwjreoiw.setLineWrap(true);
+		txtrSjhrweshjewhrehjriowejroiwjreoiw.setEditable(false);
+		txtrSjhrweshjewhrehjriowejroiwjreoiw.setBackground(SystemColor.menu);
+		txtrSjhrweshjewhrehjriowejroiwjreoiw.setBounds(21, 36, 510, 131);
+		heroesInfosPanel.add(txtrSjhrweshjewhrehjriowejroiwjreoiw);
+		
+		JLabel lblNewLabel = new JLabel("Heroes backpack");
+		lblNewLabel.setBounds(21, 0, 165, 32);
+		heroesInfosPanel.add(lblNewLabel);
+		
+		JLabel lblWallet = new JLabel("Wallet: " + manager.getSquad().getWallet() + " coins.");
+		lblWallet.setBounds(556, 41, 92, 26);
+		heroesInfosPanel.add(lblWallet);
 		
 		backToMapBtn.addActionListener(new ActionListener() {
 			
