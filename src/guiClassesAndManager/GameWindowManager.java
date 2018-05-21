c44bd86487ee98941a954fbd4397fcfd3aa97c45
Jay <janardan.hamilton@gmail.com>
2018-05-21 22:17:08 +1200

    Bugs

--- src/guiClassesAndManager/GameWindowManager.java
+++ src/guiClassesAndManager/GameWindowManager.java
@@ -43,15 +43,15 @@ public class GameWindowManager implements java.io.Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private final String characterTypes = "1. Talkitive : Has the ability to Charm people \n"
+	private final String characterTypes = "Talkitive : Has the ability to Charm people \n"
 			+ "    (Gets 25% better prices at shops) \n"
-			+ "2. Smart : Mystery Ability, which could help or hinder \n"
+			+ "Smart : Mystery Ability, which could help or hinder \n"
 			+ "    your team (Minigame dependant) \n"
-			+ "3. Practical : Gets better odds at rock, paper Scissors \n"
-			+ "4. Strong : Takes 30% less damage \n"
-			+ "5. Sly : Wins all match draws unless the Villain \n"
+			+ "Practical : Gets better odds at rock, paper Scissors \n"
+			+ "Strong : Takes 30% less damage \n"
+			+ "Sly : Wins all match draws unless the Villain \n"
 			+ "    has this abilty \n"
-			+ "6. Dog : Is a good boy (Grants all team member\n"
+			+ "Dog : Is a good boy (Grants all team member\n"
 			+ "    extra 25HP Max Health)";
 //	private HeroesSquad squad = new HeroesSquad();
 //	private ArrayList<City> world = new ArrayList<City>();
